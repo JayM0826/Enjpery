bbcf2c2705ef07e11af2263df4bfb3c7f645c0f2
J <maji91fan@gmail.com>
2017-06-15 13:17:47 +0800

    bottombar终于弄好了，就这么定下来了

--- app/src/main/java/com/j/enjpery/app/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/j/enjpery/app/ui/mainactivity/MainActivity.java
@@ -1,22 +1,41 @@
 package com.j.enjpery.app.ui.mainactivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.clans.fab.FloatingActionMenu;
 import com.j.enjpery.R;
 import com.j.enjpery.app.base.BaseActivity;
 import com.j.enjpery.app.ui.teaminfo.TeamInfoActivity;
 import com.j.enjpery.app.util.SnackbarUtil;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-public class MainActivity extends BaseActivity {
+import java.util.List;
+
+import it.sephiroth.android.library.bottomnavigation.BadgeProvider;
+import it.sephiroth.android.library.bottomnavigation.BottomBehavior;
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
+
+import static android.util.Log.INFO;
+import static it.sephiroth.android.library.bottomnavigation.MiscUtils.log;
+
+public class MainActivity extends BaseActivity implements BottomNavigation.OnMenuItemSelectionListener {
+    private BottomNavigation mBottomNavigation;
+    private SystemBarTintManager mSystemBarTint;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
@@ -32,10 +51,37 @@ public class MainActivity extends BaseActivity {
 
             }
         });
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-           ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        final FloatingActionMenu floatingActionButton = (FloatingActionMenu) findViewById(R.id.fab);
+        assert floatingActionButton != null;
+        mBottomNavigation = (BottomNavigation) findViewById(R.id.BottomNavigation);
+        floatingActionButton.findViewById(R.id.fab_item1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction(
+                        "Action",
+                        null
+                ).show();
+            }
+        });
+
+        if (null != getBottomNavigation() && null == savedInstanceState) {
+            getBottomNavigation().setDefaultSelectedIndex(0);
+            ((BottomBehavior) getBottomNavigation().getBehavior()).setOnExpandStatusChangeListener(
+                    new BottomBehavior.OnExpandStatusChangeListener() {
+                        @Override
+                        public void onExpandStatusChanged(final boolean expanded, final boolean animate) {
+                            if (expanded) {
+                                floatingActionButton.showMenu(animate);
+                            } else {
+                                floatingActionButton.hideMenu(animate);
+                            }
+                        }
+                    });
+
+            final BadgeProvider provider = getBottomNavigation().getBadgeProvider();
+            provider.show(R.id.bbn_item3);
+            provider.show(R.id.bbn_item4);
+        }
     }
 
     @Override
@@ -45,31 +91,88 @@ public class MainActivity extends BaseActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+    @Override
+    public void onMenuItemSelect(int i, int i1, boolean b) {
+        SnackbarUtil.show(mBottomNavigation, "Hello 罗志华");
+        Toast.makeText(this, "hello 罗志华", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onMenuItemReselect(int i, int i1, boolean b) {
+        SnackbarUtil.show(mBottomNavigation, "Hello 罗志华");
+        Toast.makeText(this, "hello 罗志华", Toast.LENGTH_LONG).show();
+    }
+
+    public BottomNavigation getBottomNavigation() {
+        if (null == mBottomNavigation) {
+            mBottomNavigation = (BottomNavigation) findViewById(R.id.BottomNavigation);
+        }
+        return mBottomNavigation;
+    }
+
+    public static class FabBehavior extends CoordinatorLayout.Behavior<FloatingActionMenu> {
+        public FabBehavior() {
+            super();
+        }
+
+        public FabBehavior(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    SnackbarUtil.show(mTextMessage, "ok");
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
+        public boolean layoutDependsOn(final CoordinatorLayout parent, final FloatingActionMenu child, final View dependency) {
+            if (BottomNavigation.class.isInstance(dependency)) {
+                return true;
+            } else if (Snackbar.SnackbarLayout.class.isInstance(dependency)) {
+                return true;
             }
-            return false;
+            return super.layoutDependsOn(parent, child, dependency);
         }
-    };
 
+        @Override
+        public boolean onDependentViewChanged(
+                final CoordinatorLayout parent, final FloatingActionMenu child, final View dependency) {
+            log("志华", INFO, "onDependentViewChanged: " + dependency);
 
+            final List<View> list = parent.getDependencies(child);
+            int bottomMargin = ((ViewGroup.MarginLayoutParams) child.getLayoutParams()).bottomMargin;
+
+            float t = 0;
+            boolean result = false;
+
+            for (View dep : list) {
+                if (Snackbar.SnackbarLayout.class.isInstance(dep)) {
+                    t += dep.getTranslationY() - dep.getHeight();
+                    result = true;
+                } else if (BottomNavigation.class.isInstance(dep)) {
+                    BottomNavigation navigation = (BottomNavigation) dep;
+                    t += navigation.getTranslationY() - navigation.getHeight() + bottomMargin;
+                    result = true;
+                }
+            }
+
+            child.setTranslationY(t);
+            return result;
+        }
+
+        @Override
+        public void onDependentViewRemoved(
+                final CoordinatorLayout parent, final FloatingActionMenu child, final View dependency) {
+            super.onDependentViewRemoved(parent, child, dependency);
+        }
+    }
+
+    public int getNavigationBarHeight() {
+        return getSystemBarTint().getConfig().getNavigationBarHeight();
+    }
+    public SystemBarTintManager getSystemBarTint() {
+        if (null == mSystemBarTint) {
+            mSystemBarTint = new SystemBarTintManager(this);
+        }
+        return mSystemBarTint;
+    }
 
 }
