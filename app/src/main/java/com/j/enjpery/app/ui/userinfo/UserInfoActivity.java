812693df93c1ce0881c669c8e67eb37a61181812
J <maji91fan@gmail.com>
2017-07-29 10:28:34 +0800

    使用了鲁班图片压缩，上传下载不用费大流量了

--- app/src/main/java/com/j/enjpery/app/ui/userinfo/UserInfoActivity.java
+++ app/src/main/java/com/j/enjpery/app/ui/userinfo/UserInfoActivity.java
@@ -3,6 +3,7 @@ package com.j.enjpery.app.ui.userinfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,12 +18,14 @@ import com.avos.avoscloud.SaveCallback;
 import com.bumptech.glide.Glide;
 import com.j.enjpery.R;
 import com.j.enjpery.app.base.BaseActivity;
+import com.j.enjpery.app.ui.mainactivity.MainActivity;
 import com.j.enjpery.app.util.SnackbarUtil;
 import com.jakewharton.rxbinding2.view.RxView;
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.bean.ImageItem;
 import com.lzy.imagepicker.ui.ImageGridActivity;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +33,14 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Flowable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
+import top.zibin.luban.Luban;
 
 public class UserInfoActivity extends BaseActivity {
     private static final int IMAGE_PICKER = 100;
@@ -126,31 +136,60 @@ public class UserInfoActivity extends BaseActivity {
                 List<ImageItem> images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
                 if (images != null) {
                     Timber.i(images.get(0).path);
-                    try {
-                        AVFile file = AVFile.withAbsoluteLocalPath(images.get(0).name, images.get(0).path);
-                        AVUser avUser = AVUser.getCurrentUser();
-                        avUser.put("headImage", file);
-                        avUser.saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(AVException e) {
-                                if (e == null) {
-                                    AVFile avFile = (AVFile) avUser.get("headImage");
-                                    Timber.i("上传成功" + avFile.getUrl());
-                                    SnackbarUtil.show(headImage, "上传成功");
-                                    Glide.with(UserInfoActivity.this).load(avFile.getUrl()).into(headImage);
-                                } else {
-                                    Timber.e("上传失败");
-                                    SnackbarUtil.show(headImage, "上传失败");
-                                }
-                            }
-                        });
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                        Timber.e("文件未找到");
-                    }
+                    File imgFile = new File(images.get(0).path);
+                    compressWithRx(imgFile);
                 }
             }
         }
 
     }
+
+
+    private void compressWithRx(File file) {
+        Flowable.just(file)
+                .observeOn(Schedulers.io())
+                .map(new Function<File, File>() {
+                    @Override
+                    public File apply(@NonNull File file) throws Exception {
+                        return Luban.with(UserInfoActivity.this).load(file).get();
+                    }
+                })
+                .subscribe(new Consumer<File>() {
+                    @Override
+                    public void accept(@NonNull File file) throws Exception {
+                        Timber.i("压缩", file.getAbsolutePath());
+                        uploadHeadImage(file);
+                    }
+                });
+    }
+
+    private void uploadHeadImage(File file) {
+
+        try {
+            AVFile fileInfo = AVFile.withAbsoluteLocalPath(file.getName(), file.getAbsolutePath());
+            AVUser avUser = AVUser.getCurrentUser();
+            avUser.put("headImage", fileInfo);
+            avUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(AVException e) {
+                    if (e == null) {
+                        AVFile avFile = (AVFile) avUser.get("headImage");
+                        Timber.i("上传成功" + avFile.getUrl());
+                        SnackbarUtil.show(headImage, "上传成功");
+                        Glide.with(UserInfoActivity.this).load(avFile.getUrl()).into(headImage);
+                    } else {
+                        Timber.e("上传失败");
+                        SnackbarUtil.show(headImage, "上传失败");
+                    }
+                }
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Timber.e("文件未找到");
+        }
+
+
+    }
+
+
 }
